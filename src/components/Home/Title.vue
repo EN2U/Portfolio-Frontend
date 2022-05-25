135b7d926e8e7090f5a89c3351ef32bcb4ecd4ac
Eduardo Nuez Wehbe <enuez@quisnata.com>
2022-05-25 14:45:06 +0100

    Rename index to home. Extract Home code into new Title component

---
+++ src/components/Home/Title.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <span class="text-h1 q-ma-xl">EN2U</span>
+    <div class="column justify-end full-width">
+      <span
+        class="text-weight-bolder text-italic caption text-right"
+        v-text="'Software developer'"
+      ></span>
+      <span
+        class="text-weight-bolder text-italic caption text-right"
+        v-text="'Graduated on University of La Laguna'"
+      ></span>
+      <span
+        class="text-weight-bolder text-italic caption text-right"
+        v-text="'Programming is my passion'"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  setup() {
+    return {};
+  },
+};
+</script>
