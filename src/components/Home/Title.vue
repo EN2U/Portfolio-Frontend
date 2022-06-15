6f72a56e532769549f7a7aee6c0d6bbe997be501
Eduardo Nuez Wehbe <nuezwehbe@gmail.com>
2022-06-15 12:44:58 +0100

    Develop (#2)
    * Init quasar project
    
    * Update default layout format.
    Update quasar.conf routermode and extras section including icons
    
    * Home page (#1)
    
    * Rename index to home.
    Extract Home code into new Title component
    
    * Add trading and data analyst pages.
    Add routes for new pages
    
    * Add primary and secondary color.
    Update title component to be responsive
    
    * Add Descriptionsection on the home page
    
    * Add base template of Project history component
    
    * Add Home images for technologies carousel.
    Remove lorem ipsum test with descriptions in ProjectHistory.
    Add Technology carousel structure, images and lorem ipsum text.
    
    * Remove lorem ipsum text and update it with a good one on Technology component
    
    * Merge description text with title.
    update timeline order from present to past.
    
    * Remake technology component with button style instead of caroussel
    Update assets for better technology icons.
    
    * Add Interest section.
    Add footer with contact methods
    
    * Add responsive for mobile
    
    * Add fluid heading for responsives headers and add different rating style on Interest
    
    * Refactor html code with v-for in Interest and ProjectHistory.
    Add github as communication method
    
    * Remove toolbar being fixed
    
    Co-authored-by: Eduardo Nuez Wehbe <enuez@quisnata.com>
    
    Co-authored-by: Eduardo Nuez Wehbe <enuez@quisnata.com>

---
+++ src/components/Home/Title.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row justify-around full-height items-center ">
+    <div class="col-12 col-md-6 row justify-center items-center">
+      <div class="col-12 row justify-center">
+        <span class="text-h2 col-8 col-md-6 row justify-center items-center text-weight-bolder text-white q-ma-md">
+          Full Stack Developer dedicated to Finance and Data Analysis
+        </span>
+        <span
+          class="text-weight-bolder text-italic text-white text-h6 col-8 col-md-6 text-body1"
+        >
+          I am Eduardo Nuez Wehbe, a Full Stack Developer who loves Data Analysis in the Financial sectors for Algorithmic Trading.  
+        </span>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 text-white row justify-center items-center" style="width: 50%; height: 50%">
+      <q-skeleton style="min-width: 250px; min-height: 250px; width: 70%; height: 70%">
+        fotito mia :)
+      </q-skeleton>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  setup() {
+    return {};
+  },
+};
+</script>
