7b96b7ffab5f4f5ace1b5f9854db408697a0c9a5
Eduardo Nuez Wehbe <95353696+EN2U@users.noreply.github.com>
2022-06-15 12:34:27 +0100

    Home page (#1)
    * Rename index to home.
    Extract Home code into new Title component
    
    * Add trading and data analyst pages.
    Add routes for new pages
    
    * Add primary and secondary color.
    Update title component to be responsive
    
    * Add Descriptionsection on the home page
    
    * Add base template of Project history component
    
    * Add Home images for technologies carousel.
    Remove lorem ipsum test with descriptions in ProjectHistory.
    Add Technology carousel structure, images and lorem ipsum text.
    
    * Remove lorem ipsum text and update it with a good one on Technology component
    
    * Merge description text with title.
    update timeline order from present to past.
    
    * Remake technology component with button style instead of caroussel
    Update assets for better technology icons.
    
    * Add Interest section.
    Add footer with contact methods
    
    * Add responsive for mobile
    
    * Add fluid heading for responsives headers and add different rating style on Interest
    
    * Refactor html code with v-for in Interest and ProjectHistory.
    Add github as communication method
    
    * Remove toolbar being fixed
    
    Co-authored-by: Eduardo Nuez Wehbe <enuez@quisnata.com>

---
+++ src/components/Home/ProjectHistory.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="q-px-lg q-pb-md row justify-center text-white">
+    <q-timeline class="col-8" :layout="layout" color="secondary">
+      <q-timeline-entry heading>
+        <span class="text-bold" v-text="'Projects Timeline'"></span>
+      </q-timeline-entry>
+
+      <q-timeline-entry v-for="(data,index) in timelineData" :key="index"
+        :title="data.title"
+        :subtitle="data.subtitle"
+        :side="data.side"
+        :icon="data.icon"
+        :color="data.color"
+      >
+        <span class="text-body1" v-text="data.text"></span>
+      </q-timeline-entry>
+    </q-timeline>
+  </div>
+</template>
+
+<script>
+import { useQuasar } from 'quasar'
+import { computed, ref } from 'vue'
+
+export default {
+  name: 'ProjectHistory',
+  setup () {
+    const $q = useQuasar()
+    const timelineData = ref([
+      {
+        title:"Market analyst and Portfolio management Tools",
+        subtitle:"On Going",
+        side:"left",
+        icon:"mdi-sync",
+        color: "",
+        text:"I am currently working on portfolio management tools for trading control and money management and as a technical market analyst applying machine learning concepts."
+      },
+      {
+        title:"Company change from Kaudal to Quisnatta and Atlantic-Cobalt",
+        subtitle:"September 7, 2021",
+        side:"right",
+        icon:"done_all",
+        color: "orange",
+        text:"With the end of my final degree project, I moved to Quisnatta, where I am still working as a full stack developer and as a more finance-oriented data analyst."
+      },
+      {
+        title:"End of internship and start working as an employee at Kaudal.",
+        subtitle:"June 7, 2021",
+        side:"left",
+        icon:"mdi-certificate",
+        color: "",
+        text:"I finished my internship and started to work within the company as a Full Stack Developer. I continued working on the administration project but this time with a higher position of responsibility, where I mainly dedicated myself to optimize and improve the tool, teaching colleagues the best practices and the deeper functioning of the vue framework.  "
+      },
+      {
+        title:"End of participation in the Trading project",
+        subtitle:"May 07, 2021",
+        side:"right",
+        icon:"mdi-alert",
+        color: "",
+        text:"I ended my participation in the cryptocurrency trading project due to lack of time because I had too much work due to external internships and my final degree project. This didn't keep me away from this sector. I started researching independently scientific papers about influential people in this sector (such as Marcos Lopez de Prado) and training myself in the free time I had. "
+      },
+      {
+        title:"Externships at Kaudal as Full Stack Developer",
+        subtitle:"March 22, 2021",
+        side:"left",
+        icon:"mdi-school",
+        color: "",
+        text:"I joined the company Kaudal as an intern, doing small tasks mainly oriented to solve bugs and errors in an internal administration application."
+      },
+      {
+        title:"Cryptocurrency Trading",
+        subtitle:"October 20, 2020",
+        side:"right",
+        icon:"mdi-finance",
+        color: "",
+        text:"Before the end of the last academic year, together with some fellow students, we were looking to develop a project far from what we saw in the degree. That is why I became interested in the world of blockchain and cryptocurrencies. This indirectly led me to learn about how crypto finance works and to start a trading project within this sector. "
+      },
+    ])
+
+    return {
+      timelineData,
+      layout: computed(() => {
+        return $q.screen.lt.sm ? 'dense' : ($q.screen.lt.md ? 'comfortable' : 'loose')
+      })
+    }
+  }
+}
+
+</script>
